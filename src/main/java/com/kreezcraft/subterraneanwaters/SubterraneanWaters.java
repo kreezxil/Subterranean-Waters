e584c6d8e89abd666c66c52b0d9ac2168478c35a
kreezxil <kreezxil@gmail.com>
2018-06-11 22:20:30 -0500

    1.12.2-1.3.4
    time to go bald again

--- src/main/java/com/kreezcraft/subterraneanwaters/SubterraneanWaters.java
+++ src/main/java/com/kreezcraft/subterraneanwaters/SubterraneanWaters.java
@@ -35,7 +35,7 @@ public class SubterraneanWaters {
 		Chunk theChunk = new Chunk(event.getWorld(), event.getX(), event.getZ());
 		BlockPos pos = new BlockPos(event.getX() << 4, 1, event.getZ() << 4);
 		Biome theBiome = theChunk.getBiome(pos, event.getWorld().getBiomeProvider());
-		System.out.println("Biome: ["+theBiome.getBiomeName()+"]");
+		//System.out.println("Biome: ["+theBiome.getBiomeName()+"]");
 		if (!ArrayUtils.contains(ModConfig.exclude, theBiome.getBiomeName()))
 			generator.generate(event.getPrimer(), event.getWorld(), new Random(event.getWorld().getSeed()),
 					event.getX(), event.getZ(), event.getGenerator());
